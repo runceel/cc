54b2a0d771d0d1ef40bd0a1bc74789653c658d7c
Kazuki Ota <kaota@microsoft.com>
2022-01-14 17:11:57 +0900

    PKCE

--- Source/CompanyCommunicator/Startup.cs
+++ Source/CompanyCommunicator/Startup.cs
@@ -7,6 +7,8 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator
 {
     using System;
     using System.Net;
+    using System.Net.Http;
+    using Microsoft.AspNetCore.Authentication.OpenIdConnect;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Diagnostics;
     using Microsoft.AspNetCore.Hosting;
@@ -19,6 +21,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
     using Microsoft.Graph;
+    using Microsoft.Identity.Web;
     using Microsoft.Teams.Apps.CompanyCommunicator.Authentication;
     using Microsoft.Teams.Apps.CompanyCommunicator.Bot;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Adapter;
@@ -132,6 +135,14 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator
             AuthenticationOptions authenticationOptionsParameter = new AuthenticationOptions();
             Startup.FillAuthenticationOptionsProperties(authenticationOptionsParameter, this.Configuration);
             services.AddAuthentication(this.Configuration, authenticationOptionsParameter);
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(o =>
+                {
+                    this.Configuration.GetSection("AzureAd").Bind(o);
+                    var defaultBackChannel = new HttpClient();
+                    defaultBackChannel.DefaultRequestHeaders.Add("Origin", "thisismyapp");
+                    o.Backchannel = defaultBackChannel;
+                });
             services.AddControllersWithViews();
 
             // Setup SPA static files.
